3ea13483cfd2b8baf427c26a19a8f147893f9073
Bielfla27 <gabibielmft@outlook.com>
2022-10-19 19:43:48 -0300

    Criando o primeiro controle e entendendo as requisições

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.alura.mvc.mudi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //integrando a classe com o controller
+public class HelloController {
+		
+	@GetMapping("/hello") //dizendo para o spring que quando chegar uma requisição com o método /hello chamar esse método que vai levar para view hello.html
+	public String hello(Model model) {
+		model.addAttribute("nome", "Fofão");
+		return "hello";
+	}
+}
