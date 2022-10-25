cd10797ee6072a60046986b4e1c34152bdf10a2d
Bielfla27 <gabibielmft@outlook.com>
2022-10-25 20:43:18 -0300

    Pecorrendo lista na view

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,28 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String home(){
+	public String home(Model model){
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xbox");
+		pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_2X_627914-MLA40655732617_022020-V.webp");
+		pedido.setUrlProduto("https://www.mercadolivre.com.br/microsoft-xbox-one-s-1tb-standard-cor-branco/p/MLB14114827");
+		pedido.setDescricao("Xbox o melhor sem duvida");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedidos);
+		
 		return "home";
 	}
 }
