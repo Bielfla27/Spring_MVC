d3fc2005b0762e4cb13a70d5709cdd6fea933d69
Bielfla27 <gabibielmft@outlook.com>
2022-10-25 11:26:54 -0300

    Criando o modelo de view

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller //integrando a classe com o controller
 public class HelloController {
 		
-	@GetMapping("/hello") //dizendo para o spring que quando chegar uma requisição com o método /hello chamar esse método que vai levar para view hello.html
+	@GetMapping("/hello") //dizendo para o spring que quando chegar uma requisição com o método /hello  a Action @GetMapping chamar esse método que vai levar para view hello.html
 	public String hello(Model model) {
 		model.addAttribute("nome", "Fofão");
 		return "hello";
