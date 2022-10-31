bbcec9833da9ca56151d554d7f80be5ae6e1dcb3
Bielfla27 <gabibielmft@outlook.com>
2022-10-31 20:33:06 -0300

    Salvando Pedido no banco

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,32 @@
 package br.com.alura.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidosRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
+	
+	@Autowired
+	private PedidosRepository pedidosRepository;
 
 	@GetMapping("formulario")
 	public String formulario() {
-		return "pedido/formulario";
+	return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido novoPedido) {
+		Pedido pedido = novoPedido.toPedido();
+		pedidosRepository.save(pedido);
+		return "";
 	}
 }
+
