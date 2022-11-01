ae995ff56e71f57a1ecc981fa9cc7f413052ea9e
Bielfla27 <gabibielmft@outlook.com>
2022-10-31 21:00:13 -0300

    Adicionando validação dos campos com java

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,15 @@ public class PedidoController {
 	private PedidosRepository pedidosRepository;
 
 	@GetMapping("formulario")
-	public String formulario() {
+	public String formulario(RequisicaoNovoPedido novoPedido) {
 	return "pedido/formulario";
 	}
 	
 	@PostMapping("novo")
-	public String novo(RequisicaoNovoPedido novoPedido) {
+	public String novo(@Valid RequisicaoNovoPedido novoPedido, BindingResult result) {
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
 		Pedido pedido = novoPedido.toPedido();
 		pedidosRepository.save(pedido);
 		return "";
