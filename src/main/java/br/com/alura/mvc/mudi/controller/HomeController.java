df5cf6cc772cae98f0a01a1bbb1025ba510a54af
Bielfla27 <gabibielmft@outlook.com>
2022-11-01 22:57:47 -0300

    Adicionando rotas para menu e tratando elas

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidosRepository;
+import ch.qos.logback.core.status.Status;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired //Ela serve para você pedir para o Spring – “eu quero uma instância de ‘PedidoRepository’!” 
 	private PedidosRepository pedidoRepository;
 
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model){
 	
 		List<Pedido> pedidos = pedidoRepository.findAll();
@@ -25,4 +31,19 @@ public class HomeController {
 		//model.getAttribute(null); posso confirir e pegar os parêmetros pelo id assim 
 		return "home";
 	}
+	
+	@GetMapping("/{status}") 
+	public String porStatus(@PathVariable("status") String status, Model model){
+	
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("status", status);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
+	}
 }
+
--- src/main/java/br/com/alura/mvc/mudi/repository/PedidosRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidosRepository.java
@@ -10,9 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
 
 @Repository //Estou dizendo para o Spring – “essa classe aqui é um repositório e eu quero que você gerencie essa classe e crie instâncias toda vez que alguém pedir” 
 public interface PedidosRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 
 	
 }
