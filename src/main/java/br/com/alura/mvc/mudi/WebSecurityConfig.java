cd7e48978d765462a4510c8637886338c30bfe92
Bielfla27 <gabibielmft@outlook.com>
2022-11-05 11:07:17 -0300

    Usando jdbc authetication para fazer o login na aplicação

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,19 +1,28 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired //Ele serve para você pedir para o Spring – “eu quero uma instância de ‘DataSource’!” 
+	private DataSource dataSource;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,23 +33,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.formLogin((form) -> form   
 				.loginPage("/login") //dizendo para redirecionar para a página /login para fazer o login
-				.permitAll())// dando permição para todos os usuarios 
+				.defaultSuccessUrl("/home", true) //dizendo que toda vez que o usario fizer o login corretamente vá para página home
+				.permitAll()) // dando permição para todos os usuarios 
 		.logout(logout -> logout.logoutUrl("/logout")); //deslogando o usuario 
 		
 	}
 	
-	
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("gabriel")
-				.password("gabriel")
-				.roles("ADM")
-				.build();
 
-		return new InMemoryUserDetailsManager(user);
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
 	}
 
 }
