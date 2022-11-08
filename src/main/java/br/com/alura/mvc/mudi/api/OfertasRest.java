08b662b35858020a87aecb20bb03aed0d8411067
Bielfla27 <gabibielmft@outlook.com>
2022-11-08 03:00:21 -0300

    Guardando ofertas

---
+++ src/main/java/br/com/alura/mvc/mudi/api/OfertasRest.java
@@ -0,0 +1,36 @@
+package br.com.alura.mvc.mudi.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovaOferta;
+import br.com.alura.mvc.mudi.model.Oferta;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidosRepository;
+
+@RestController
+@RequestMapping("/api/ofertas")
+public class OfertasRest {
+	
+	@Autowired //Ela serve para você pedir para o Spring – “eu quero uma instância de ‘PedidoRepository’!” 
+	private PedidosRepository pedidoRepository;
+	
+	@PostMapping
+	public Oferta criaOferta(RequisicaoNovaOferta requisicao ) {
+		Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoId());
+		if(pedidoBuscado.isPresent()) {
+			return null; 
+		}
+		Pedido pedido = pedidoBuscado.get();
+		Oferta nova = requisicao.toOferta();
+		nova.setPedido(pedido);
+		pedido.getOfertas().add(nova);
+		pedidoRepository.save(pedido);
+		return nova;
+	}
+
+}
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -2,7 +2,9 @@ package br.com.alura.mvc.mudi.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,6 +13,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Pedido {
@@ -26,7 +31,10 @@ public class Pedido {
 	@Enumerated(EnumType.STRING)
 	private StatusPedido status;
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore // para não da o erro quando o json vim pegar os dados
 	private User user;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pedido", fetch = FetchType.LAZY)
+	private List<Oferta> ofertas;
 	
 	public String getNomeProduto() {
 		return nomeProduto;
@@ -80,7 +88,13 @@ public class Pedido {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	public List<Oferta> getOfertas() {
+		return ofertas;
+	}
+	public void setOfertas(List<Oferta> ofertas) {
+		this.ofertas = ofertas;
+	}
 	
 	
-
+	
 }
